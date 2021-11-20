9dcedd11e0641e063727032ea28411a1012bdd79
songyikim0911 <ksi6470@gmail.com>
2021-11-20 22:13:22 +0900

    A

---
+++ src/main/java/com/example/bitboardpractice/controller/ListController.java
@@ -0,0 +1,10 @@
+package com.example.bitboardpractice.controller;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.annotation.WebServlet;
+
+@Log4j2
+@WebServlet(name = "ListController", value = "/board/list")
+public class ListController {
+}
--- src/main/java/com/example/bitboardpractice/controller/UploadController.java
+++ src/main/java/com/example/bitboardpractice/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.example.bitboardpractice.controller;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -22,35 +23,58 @@ public class UploadController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uploadFolder = "C:\\upload";
+        byte[] buffer = new byte[1024*8];
 
-    String uploadFolder = "C:\\upload";
-    byte[] buffer = new byte[1024*8];
 
+        Collection<Part> parts = request.getParts();
 
-    Collection<Part> parts = request.getParts();
+        parts.forEach(part -> {
 
-    parts.forEach(part -> {
+            String type = part.getContentType();
+            if(type == null){
+                //null 파일이 아니라는 것.
+                log.info("partName:" + part.getName());
+                return;
+            }
 
-        String type = part.getContentType();
-        if(type == null){
-            log.info("partName"+part.getName());
-            return;
-        }
-        String fileName = part.getSubmittedFileName();
+            String fileName = part.getSubmittedFileName();
 
-        String uploadFileName = System.currentTimeMillis()+"_"+fileName;
-        log.info(fileName);
+            String uploadFileName = System.currentTimeMillis()+"_"+fileName;
+            log.info(fileName);
 
-          try(InputStream in = part.getInputStream();
+            //원본 파일 저장
+        try (InputStream in = part.getInputStream();
              OutputStream fos = new FileOutputStream(uploadFolder+ File.separator+ uploadFileName);
-        ){
-              while(true){
-                  int count = in.read(buffer);
-              }
-          }
+             //중복 파일 구분을 위한 코드 추가.
+
+        ) {
+            while(true){
+                int count = in.read(buffer);
+                if(count == -1){ break; }
+                fos.write(buffer, 0, count);
+            }
+        }catch(Exception e){
+
+        }//원본파일저장 끝
+
+        //이미지에 대해서만 섬네일
+        if(type.startsWith("image")){
+
+            try {
+                Thumbnails.of(new File(uploadFolder+ File.separator+ uploadFileName))
+                        .size(100, 100)
+                        .toFile(new File(uploadFolder+ File.separator+ "s_"+uploadFileName));
+            }catch(Exception e){
+
+            }
+        }
 
 
-    })
+        log.info("---------------------------");
+
+    });//for each
+
 
     }
 }
